f7fa4ed9695bc80e7f0f26358fe0f381e607340e
Dafluxtech <dayodeji31@yahoo.com>
2022-12-01 10:19:38 +0000

    task 9 err fixed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 9-print_last_digit.py
-# Eden Ayodeji <dayodeji31@yahoo.com>
 
-
-def print_last_digit(number);
-"""Print the last digit of a number and return it."""
-print(abs(number) % 10, end="")
-return (abs(number) % 10)
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)turn (abs(number) % 10)

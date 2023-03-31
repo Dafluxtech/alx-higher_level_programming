2f8dd380f6794d9db6f0522b8b7cf611fbe7a553
Dafluxtech <dayodeji31@yahoo.com>
2023-03-31 13:27:43 +0000

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-    Fetche https://intranet.hbtn.io/status
-"""
-
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
+
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
